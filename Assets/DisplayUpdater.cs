e10dc51cc0d9e14b491c5a3b5e0d1eb89dcbd557
AdVdTools <ngemonji@gmail.com>
2016-11-26 20:24:52 +0100

    First Commit
    Display that can cycle through many info sections.

---
+++ Assets/DisplayUpdater.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(UtilityDisplay))]
+public class DisplayUpdater : MonoBehaviour {
+	UtilityDisplay.DisplayInfo fpsInfo;
+	UtilityDisplay.DisplayInfo unityInfo;
+
+	// Use this for initialization
+	void Start () {
+		GetSections();
+	}
+
+	void GetSections() {
+		UtilityDisplay display = GetComponent<UtilityDisplay>();
+		fpsInfo = display.GetSectionById("fps");
+		fpsInfo.anchor = TextAnchor.UpperCenter;
+
+		unityInfo = display.GetSectionById("unity");
+		unityInfo.anchor = TextAnchor.LowerLeft;
+	}
+
+	float deltaTime = 0.0f;
+	void Update() {
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+
+		float msec = deltaTime * 1000f;
+		float fps = 1f / deltaTime;
+		fpsInfo.text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+		fpsInfo.textSize = (int)(Screen.height / 25f);
+		fpsInfo.color = fps < 30f ? Color.red : Color.green;
+	}
+
+
+	#if UNITY_EDITOR
+	void OnValidate() {
+		GetSections();
+		unityInfo.text = Application.unityVersion+"\n"
+			+ UnityEditor.EditorUserBuildSettings.activeBuildTarget +"\n"
+//			+ GetConsoleOutput()+"\n"
+			+ GetFileContent("test.txt")+"\n"
+			+ System.Environment.GetEnvironmentVariable("PATH");
+		Debug.Log("OnValidate "+UnityEditor.EditorUserBuildSettings.activeBuildTarget+" "+UnityEditor.BuildPipeline.isBuildingPlayer);
+//		RunCMD();
+    }
+
+	void RunCMD() {
+		if (!UnityEditor.BuildPipeline.isBuildingPlayer) return;
+
+		System.Diagnostics.Process p = new System.Diagnostics.Process();
+		p.StartInfo.FileName = "CMD.exe";
+		p.StartInfo.Arguments = "pwd > Assets/testFile.txt";
+		p.Start();
+		p.WaitForExit();
+	}
+
+	string GetFileContent(string path) {
+		if (System.IO.File.Exists(path)) {
+			return System.IO.File.ReadAllText(path);
+		}
+		else {
+			return path+" not found";
+		}
+	}
+
+	string GetConsoleOutput() {
+		if (!UnityEditor.BuildPipeline.isBuildingPlayer) return "";
+
+		System.Diagnostics.Process p = new System.Diagnostics.Process();
+		p.StartInfo.UseShellExecute = false;
+		p.StartInfo.RedirectStandardOutput = true;
+		p.StartInfo.FileName = "CMD.exe";
+		p.StartInfo.Arguments = "pwd";
+		p.Start();
+
+		string output = p.StandardOutput.ReadToEnd();
+		p.WaitForExit();
+		return output;
+	}
+	#endif
+}
---
+++ Assets/UtilityDisplay.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class UtilityDisplay : MonoBehaviour {
+
+	enum Step { ShortTap, LongTap }
+	[SerializeField]
+	Step[] switchSequence;
+	int currentStep = 0;
+	float lastEventTime;
+	bool stepRunning;
+
+	[SerializeField]
+	float maxStepDelay = 0.5f;
+	[SerializeField]
+	float longTapLength = 0.5f;
+
+
+
+	void Update() {
+		if (switchSequence != null && switchSequence.Length > 0) {
+			Step current = switchSequence[currentStep];
+			float currentTime = Time.unscaledTime;
+			bool mouse = Input.GetMouseButton(0);
+			switch (current) {
+			case Step.ShortTap:
+				if (!stepRunning) {
+					if (mouse) {
+						lastEventTime = currentTime;
+						stepRunning = true;
+					}
+				}
+				else {
+					if (!mouse){
+						if (currentTime > lastEventTime + longTapLength) {//Failed
+							currentStep = 0;
+						}
+						else {
+							currentStep++;
+						}
+						lastEventTime = currentTime;
+						stepRunning = false;
+					}
+				}
+				break;
+			case Step.LongTap:
+				if (!stepRunning) {
+					if (mouse) {
+						lastEventTime = currentTime;
+						stepRunning = true;
+					}
+				}
+				else {
+					if (!mouse){
+						if (currentTime < lastEventTime + longTapLength) {//Failed
+							currentStep = 0;
+						}
+						else {
+							currentStep++;
+						}
+						lastEventTime = currentTime;
+						stepRunning = false;
+					}
+				}
+				break;
+			}
+			if (currentStep >= switchSequence.Length) {
+				currentStep = 0;
+				sectionIndex = (sectionIndex+1)%sections.Count;
+			}
+			if (!stepRunning && currentTime > lastEventTime+maxStepDelay) {//Sequence stopped
+				currentStep = 0;
+			}
+		}
+	}
+
+	[System.Serializable]
+	public class DisplayInfo {
+		public string id;
+		[Multiline] public string text;
+		public TextAsset textFile;
+		public TextAnchor anchor = TextAnchor.LowerLeft;
+		public int textSize = 12;
+		public Color color = Color.black;
+
+		public override string ToString ()
+		{
+			return text + (textFile != null ? textFile.text : "");
+		}
+	}
+
+	GUIStyle style = new GUIStyle();
+	[HideInInspector]
+	public int sectionIndex;
+	public List<DisplayInfo> sections = new List<DisplayInfo>();
+
+	void OnGUI() {
+		if (sectionIndex >= sections.Count || sectionIndex < 0) return;
+
+		int w = Screen.width, h = Screen.height;
+		Rect rect = new Rect(0f, 0f, w, h);
+		DisplayInfo info = sections[sectionIndex];
+
+		style.alignment = info.anchor;
+		style.fontSize = info.textSize;
+		style.normal.textColor = info.color;
+
+		GUI.Label(rect, info.ToString(), style);
+	}
+
+	public DisplayInfo GetSectionById(string id) {
+		DisplayInfo info = sections.Find((section) => section.id == id);
+		if (info == null) {
+			sections.Add(info = new DisplayInfo());
+			info.id = id;
+		}
+		return info;
+	}
+
+//	#if UNITY_EDITOR
+//	public bool timeScaleToggle;
+//	[Range(0f,2f)]
+//	public float timeScale;
+//
+//	void OnValidate() {
+//		if (timeScaleToggle) {
+//			Time.timeScale = timeScale;
+//		}
+//		else {
+//			Time.timeScale = 1f;
+//		}
+//	}
+//	#endif
+}
